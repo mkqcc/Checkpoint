f64523559d43683e4e547d50879eb66343a5d1ff
mkqcc <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-07-30 14:52:09 -0400

    Finished High Order Functions

--- HigherOrderFunctions/index.js
+++ HigherOrderFunctions/index.js
@@ -4,19 +4,16 @@ PLEASE DO NOT CHANGE OR TOUCH THEM THEY WILL CAUSE ERRORS ON
 YOUR CONSOLE, BUT IT WILL NOT STOP THE REST OF YOUR CODE
 ALL VARIABLES AND FUNCTION HAVE BEEN DECLARED FOR YOU!!!!
 PLEASE ONLY TYPE WHERE 'YOUR CODE HERE' IS
-
-
 */
 
 /*
 QUESTION 1
 
 CREATE A FUNCTION THAT TAKES A CALLBACK AND INVOKES THAT CALLBACK FUNCTION
-
 */
 
 function higherOrder(callback) {
-  // CODE HERE
+    callback()
 }
 
 /*
@@ -27,7 +24,7 @@ CREATE A FUNCTION ACCEPTS ONE ARGUMENT, A CALLBACK THAT RETURNS THE RETURN VALUE
 */
 
 function q2(callback) {
-  // CODE HERE
+    return callback()
 }
 
 /*
@@ -40,14 +37,15 @@ USE A FOR-LOOP
 */
 
 function forEach(array, callback) {
-  // CODE HERE
+    for (let i = 0; i < array.length; i++) {
+        array[i] = callback(array[i]);
+    }
 }
 
 /*
 QUESTION 4
 
 CREATE A FUNCTION THAT TAKES TWO ARGUMENTS AN ARRAY AND A CALLBACK FUNCTION. IT APPLIES THE CALLBACK FUNCTION ON EACH ELEMENT AND THEN STORES THE RETURN VALUE IN A NEW ARRAY
-
 
 THIS IS JUST LIKE Array.prototype.map
 
@@ -56,15 +54,15 @@ USE A FOR-LOOP
 */
 
 function map(array, callback) {
-  // CODE HERE
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        result[i] = callback(array[i])
+    }
+    return result
 }
 
 /*
 TEST SECTION, PLEASE DO NOT TOUCH
-
-
-
-
 */
 
 const Mocha = require('mocha');
@@ -74,13 +72,13 @@ const runner = new Mocha({});
 runner.addFile('./test.js');
 
 runner.run((failures) => {
-  // if (failures) {
-  //   console.error(failures)
-  // } else {
-  //   console.log('All passed.')
-  // }
+    // if (failures) {
+    //   console.error(failures)
+    // } else {
+    //   console.log('All passed.')
+    // }
 });
 
-module.exports = { higherOrder, q2, forEach, map };
+module.exports = {higherOrder, q2, forEach, map};
 
 console.log('');
